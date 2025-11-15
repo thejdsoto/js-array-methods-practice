edac6c65068b5834b911d449957098d91061e5cd
Jade <thedavidsoto@gmail.com>
2025-11-15 12:24:21 +0800

    Add solution to filterRangeInPlace problem

---
+++ Filter range 'in place'/filterRangeInPlace.js
@@ -0,0 +1,15 @@
+function filterRangeInPlace(arr, a, b) {
+
+    for (let i = 0; i < arr.length; i++) {
+        let ele = arr[i];
+
+        if (a > ele || ele > b) {
+            arr.splice(i, 1);
+            i--;
+        }
+    }
+}
+
+let arr = [5, 3, 8, 1];
+filterRangeInPlace(arr, 1, 4); // removed the numbers except from 1 to 4
+console.log(arr);
